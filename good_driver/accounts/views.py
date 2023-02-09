22cae63928afda6f272d3faf7ee61f2a2e80aa92
TJ Heffner <aheffne@clemson.edu>
2023-02-09 20:08:54 +0000

    Added 7 files to /good_driver/accounts

---
+++ good_driver/accounts/views.py
@@ -0,0 +1,34 @@
+
+# Create your views here.
+from django.contrib import messages
+from django.shortcuts import render, redirect
+
+def register(request):
+    if request.method == 'POST':
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        confirm_password = request.POST['confirm_password']
+
+        if password==confirm_password:
+            if User.objects.filter(username=username).exists():
+                messages.info(request, 'Username is already taken')
+                return redirect(register)
+            elif User.objects.filter(email=email).exists():
+                messages.info(request, 'Email is already taken')
+                return redirect(register)
+            else:
+                user = User.objects.create_user(username=username, password=password, 
+                                        email=email, first_name=first_name, last_name=last_name)
+                user.save()
+                
+                return redirect('login_user')
+
+
+        else:
+            messages.info(request, 'Both passwords are not matching')
+            return redirect(register)
+    else:
+        return render(request, 'registration.html')
