9883dc803cf4ec458a5fc7b8fd58c3c6162fecf4
Kevin Le <le5@clemson.edu>
2023-02-20 00:18:37 +0000

    Added views.py to able to render to application

---
+++ good_driver/good_driver/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+
+from django.views.generic import TemplateView
+
+def resetPassword(request):
+    return render(request, 'resetPassword.html')
+
+def sponsorView(request):
+    return render(request, 'sponsorView.html')
+
+def pointChange(request):
+    return render(request, 'pointChange.html')
+
+def catalog(request):
+    return render(request, 'catalog.html')
+
+def pointHistory(request):
+    return render(request, 'pointHistory.html')
