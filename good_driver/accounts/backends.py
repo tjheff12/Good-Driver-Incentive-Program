2a92199b9d6f30e6c201f34bd0cb93b4af66cdc6
TJ Heffner <tjheff12@gmail.com>
2023-02-27 23:26:33 -0500

    Why can't git track files automatically

---
+++ good_driver/accounts/backends.py
@@ -0,0 +1,29 @@
+
+from . import models
+
+class CustomAuthBackend(object):
+	
+ 
+	def authenticate( username=None, password=None):
+		import hashlib
+		""" Authenticate a user based on email address as the user name. """
+		print(username)
+		print(password)
+		password_hash = hashlib.md5(password.encode()).hexdigest()
+		try:
+			user = models.Users.objects.get(email=username)
+			print("AAAAAA")
+			print(user)
+			print(user.password)
+			print("AAAAAAA")
+			if user.password == password_hash:
+				return user
+		except models.Users.DoesNotExist:
+				return None
+
+	def get_user(self, user_id):
+		""" Get a User object from the user_id. """
+		try:
+			return models.Users.objects.get(pk=user_id)
+		except models.Users.DoesNotExist:
+			return None
