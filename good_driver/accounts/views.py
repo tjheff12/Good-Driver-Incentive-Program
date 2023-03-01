833320c066a58507eb20bc2001e0a75dbb199c88
Hayden Strong <haydenstrong18@gmail.com>
2023-03-01 12:25:43 -0500

    Added sponsorPanel for sponsor users to be able to add new users to their Sponsor company

--- good_driver/accounts/views.py
+++ good_driver/accounts/views.py
@@ -186,3 +186,67 @@ def admin_delete_account(request):
             raise Http404
     # Returns 403 Error (Permission Denied)    
     else: raise PermissionDenied
+
+def sponsor_panel(request):
+    # Denies permission to ANYONE who is NOT signed in
+    if request.user.is_anonymous == True:
+        raise PermissionDenied
+    # Logic for if a user is signed in AND is of the 'Admin' type
+    elif request.user.user_type == "Sponsor":
+        if request.method == "GET":
+            # Give page with button options of letting the admin choose what action they want to take:
+                #   - Create Account (Sponsor)
+            return render(request, 'sponsorPanel.html')
+        else: raise Http404
+    # Returns 403 Error (Permission Denied)    
+    else: raise PermissionDenied
+
+def sponsor_create_account(request):
+    # Denies permission to ANYONE who is NOT signed in
+    if request.user.is_anonymous == True:
+        raise PermissionDenied
+    # Logic for if a user is signed in AND is of the 'Admin' type
+    elif request.user.user_type == "Sponsor":
+        if request.method == "GET":
+            # Give page with basically a registration page, but with more details available
+            return render(request, 'sponsorCreateAccount.html')
+        elif request.method == "POST":
+            import hashlib
+            Sponsor = models.Sponsor.objects.get(sponsor_id=models.SponsorUser.objects.get(user_id=request.user.user_id).sponsor_id)
+
+            first_name = request.POST['first_name']
+            last_name = request.POST['last_name']
+            ##username is the email
+            username = request.POST['username']
+            password = request.POST['password']
+            confirm_password = request.POST['confirm_password']
+            street_addr = request.POST['street_address']
+            city = request.POST['city']
+            zip_code = request.POST['zip_code']
+            phone_num = request.POST['phone_num']
+            
+            password_hash = hashlib.md5(password.encode()).hexdigest()
+            if password==confirm_password:
+                if models.Users.objects.filter(email=username).exists():
+                    messages.info(request, 'Username is already taken')
+                    return redirect(sponsor_create_account)
+                else:
+                    # Add to Users table
+                    user = models.Users(email=username, password=password_hash, 
+                                            first_name=first_name, last_name=last_name, street_address=street_addr, 
+                                            street_address_2=street_addr, city=city, zip_code=zip_code, 
+                                            phone_number=phone_num, user_type="Sponsor")
+                    user.save()
+
+                    # Get user id just created from auto-increment, and create a new entry matching it inside sponsor users table
+                    usersID = models.Users.objects.get(email=username).user_id
+                    sponsorUser = models.SponsorUser(user=user, sponsor=Sponsor)
+                    sponsorUser.save()
+                    
+                    return redirect('done')
+            else:
+                messages.info(request, 'Both passwords are not matching')
+                return redirect(sponsor_create_account)
+        else: raise Http404
+    # Returns 403 Error (Permission Denied)    
+    else: raise PermissionDenied
