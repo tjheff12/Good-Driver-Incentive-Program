31e6e7c63d52f232721bfe2df9a72b870a212274
TJ Heffner <aheffne@clemson.edu>
2023-03-02 21:19:33 +0000

    Updated CreateTriggers.sql

--- Database/CreateTriggers.sql
+++ Database/CreateTriggers.sql
@@ -9,3 +9,9 @@ AFTER UPDATE ON `users`
 FOR EACH ROW 
 INSERT INTO login_attempt(User_ID,Date_Time,Was_Accepted) 
 VALUES (NEW.User_ID,NEW.last_login,1);
+
+CREATE TRIGGER `update_state_change_for_new_application` 
+AFTER INSERT ON `driver_application` 
+FOR EACH ROW INSERT INTO 
+application_state_change (Application_ID,Date_Time,New_Status,New_Reason) 
+VALUES (NEW.Application_ID,NEW.Date_Time,NEW.Status,NEW.Reason);
