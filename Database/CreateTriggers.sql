8c843bd0c6292e40daa2936ff63600e15869bcaf
TJ Heffner <aheffne@clemson.edu>
2023-03-08 22:32:50 +0000

    Updated CreateTriggers.sql

--- Database/CreateTriggers.sql
+++ Database/CreateTriggers.sql
@@ -4,11 +4,16 @@ FOR EACH ROW INSERT INTO
 application_state_change (Application_ID,Date_Time,New_Status,New_Reason) 
 VALUES (NEW.Application_ID,NEW.Date_Time,NEW.Status,NEW.Reason);
 
+--Yes DELIMITER is neccesary. No, I don't know why. All I know is the if statement breaks everything without it
+DELIMITER $$
 CREATE TRIGGER `update_login_attempt` 
-AFTER UPDATE ON `users` 
-FOR EACH ROW 
-INSERT INTO login_attempt(User_ID,Date_Time,Was_Accepted) 
-VALUES (NEW.User_ID,NEW.last_login,1);
+AFTER UPDATE ON `users` FOR EACH ROW BEGIN
+IF (NEW.last_login != OLD.last_login) 
+THEN
+	INSERT INTO `login_attempt`(User_ID,Date_Time,Was_Accepted) VALUES (NEW.User_ID,NEW.last_login,1);
+END IF;
+END$$
+DELIMITER ;
 
 CREATE TRIGGER `update_state_change_for_new_application` 
 AFTER INSERT ON `driver_application` 
