fab1d1bb7ae0ae9e28cc695efe055a14dd5f60ed
Hayden Strong <haydenstrong18@gmail.com>
2023-04-19 12:50:58 -0400

    Added final ERD and db scripts that reflect our final db info

--- Database/CreateTables.sql
+++ Database/CreateTables.sql
@@ -1,152 +1,211 @@
-CREATE SCHEMA Good_Driver;
+CREATE DATABASE `Good_Driver` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
 USE Good_Driver;
 
-CREATE TABLE sponsor (
-   Sponsor_ID INTEGER PRIMARY KEY AUTO_INCREMENT,
-   Point_Value DECIMAL(10,2) NOT NULL,
-   Name VARCHAR(40) NOT NULL
-);
+CREATE TABLE `sponsor` (
+  `Sponsor_ID` int NOT NULL AUTO_INCREMENT,
+  `Point_Value` decimal(10,2) NOT NULL,
+  `Name` varchar(40) NOT NULL,
+  `Auto_Points` int DEFAULT '0',
+  `Max_Price` decimal(10,2) DEFAULT '999999.00',
+  PRIMARY KEY (`Sponsor_ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE TABLE users (
-   User_ID INTEGER PRIMARY KEY AUTO_INCREMENT,
-   First_Name VARCHAR(20) NOT NULL, 
-   Last_Name VARCHAR(20) NOT NULL,
-   Street_Address VARCHAR(80),
-   Street_Address_2 VARCHAR(80),
-   City VARCHAR(30),
-   ZIP_Code INTEGER,
-   Phone_Number VARCHAR(15),
-   Email VARCHAR(100) NOT NULL UNIQUE,
-   "Password" VARCHAR(100) NOT NULL,
-   User_Type VARCHAR(15) NOT NULL,
-   last_login DATETIME
-);
+CREATE TABLE `users` (
+  `User_ID` int NOT NULL AUTO_INCREMENT,
+  `First_Name` varchar(20) NOT NULL,
+  `Last_Name` varchar(20) NOT NULL,
+  `Street_Address` varchar(80) DEFAULT NULL,
+  `Street_Address_2` varchar(80) DEFAULT NULL,
+  `City` varchar(30) DEFAULT NULL,
+  `ZIP_Code` int DEFAULT NULL,
+  `Phone_Number` varchar(15) DEFAULT NULL,
+  `User_Type` varchar(15) NOT NULL,
+  `Email` varchar(100) NOT NULL,
+  `Password` varchar(100) NOT NULL,
+  `last_login` datetime DEFAULT NULL,
+  `is_impersonation` tinyint(1) DEFAULT '0',
+  `Security_Question_Answer` varchar(35) NOT NULL,
+  PRIMARY KEY (`User_ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=105 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 /*User Subtypes*/
-CREATE TABLE admin_user (
-   User_ID INTEGER PRIMARY KEY,
-   FOREIGN KEY  (User_ID) REFERENCES users(User_ID)
-);
-CREATE TABLE driver_user (
-   User_ID INTEGER PRIMARY KEY,
-   FOREIGN KEY  (User_ID) REFERENCES users(User_ID)
-);
-CREATE TABLE sponsor_user (
-   User_ID INTEGER PRIMARY KEY,
-   Sponsor_ID INTEGER NOT NULL,
-   FOREIGN KEY  (User_ID) REFERENCES users(User_ID),
-   FOREIGN KEY  (Sponsor_ID) REFERENCES sponsor(Sponsor_ID)
-);
+CREATE TABLE `sponsor_user` (
+  `User_ID` int NOT NULL,
+  `Sponsor_ID` int NOT NULL,
+  PRIMARY KEY (`User_ID`),
+  KEY `sponsor_user_ibfk_2` (`Sponsor_ID`),
+  CONSTRAINT `sponsor_user_ibfk_1` FOREIGN KEY (`User_ID`) REFERENCES `users` (`User_ID`) ON DELETE CASCADE ON UPDATE RESTRICT,
+  CONSTRAINT `sponsor_user_ibfk_2` FOREIGN KEY (`Sponsor_ID`) REFERENCES `sponsor` (`Sponsor_ID`) ON DELETE CASCADE ON UPDATE RESTRICT
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE TABLE driver_application (
-   Application_ID INTEGER PRIMARY KEY AUTO_INCREMENT,
-   Driver_ID INTEGER NOT NULL,
-   Sponsor_ID INTEGER NOT NULL,
-   Date_Time DATETIME NOT NULL,
-   Status VARCHAR(15) NOT NULL,
-   Reason VARCHAR(30),
-   FOREIGN KEY  (Driver_ID) REFERENCES users(User_ID),
-   FOREIGN KEY  (Sponsor_ID) REFERENCES sponsor(Sponsor_ID)
-);
+CREATE TABLE `driver_application` (
+  `Application_ID` int NOT NULL AUTO_INCREMENT,
+  `Driver_ID` int NOT NULL,
+  `Sponsor_ID` int NOT NULL,
+  `Date_Time` datetime NOT NULL,
+  `Status` varchar(15) NOT NULL,
+  `Reason` varchar(30) DEFAULT NULL,
+  PRIMARY KEY (`Application_ID`),
+  KEY `driver_application_ibfk_1` (`Driver_ID`),
+  KEY `driver_application_ibfk_2` (`Sponsor_ID`),
+  CONSTRAINT `driver_application_ibfk_1` FOREIGN KEY (`Driver_ID`) REFERENCES `users` (`User_ID`) ON DELETE CASCADE ON UPDATE RESTRICT,
+  CONSTRAINT `driver_application_ibfk_2` FOREIGN KEY (`Sponsor_ID`) REFERENCES `sponsor` (`Sponsor_ID`) ON DELETE CASCADE ON UPDATE RESTRICT
+) ENGINE=InnoDB AUTO_INCREMENT=38 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE TABLE driver_sponsor (
-   User_ID INTEGER NOT NULL,
-   Sponsor_ID INTEGER NOT NULL,
-   PRIMARY KEY(User_ID, Sponsor_ID),
-   FOREIGN KEY (User_ID) REFERENCES users(User_ID),
-   FOREIGN KEY (Sponsor_ID) REFERENCES sponsor(Sponsor_ID)
-);
+CREATE TABLE `driver_sponsor` (
+  `ID` int NOT NULL AUTO_INCREMENT,
+  `User_ID` int NOT NULL,
+  `Sponsor_ID` int NOT NULL,
+  PRIMARY KEY (`ID`),
+  KEY `driver_sponsor_ibfk_2` (`Sponsor_ID`),
+  KEY `driver_sponsor_ibfk_1` (`User_ID`),
+  CONSTRAINT `driver_sponsor_ibfk_1` FOREIGN KEY (`User_ID`) REFERENCES `users` (`User_ID`) ON DELETE CASCADE ON UPDATE RESTRICT,
+  CONSTRAINT `driver_sponsor_ibfk_2` FOREIGN KEY (`Sponsor_ID`) REFERENCES `sponsor` (`Sponsor_ID`) ON DELETE CASCADE ON UPDATE RESTRICT
+) ENGINE=InnoDB AUTO_INCREMENT=59 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE TABLE login_attempt (
-   Attempt_ID INTEGER PRIMARY KEY AUTO_INCREMENT,
-   User_ID INTEGER NOT NULL,
-   Date_Time DATETIME NOT NULL,
-   Was_Accepted BIT NOT NULL,
-   FOREIGN KEY  (User_ID) REFERENCES users(User_ID)
-);
+CREATE TABLE `login_attempt` (
+  `Attempt_ID` int NOT NULL AUTO_INCREMENT,
+  `User_ID` int NOT NULL,
+  `Date_Time` datetime NOT NULL,
+  `Was_Accepted` bit(1) NOT NULL,
+  PRIMARY KEY (`Attempt_ID`),
+  KEY `login_attempt_ibfk_1` (`User_ID`),
+  CONSTRAINT `login_attempt_ibfk_1` FOREIGN KEY (`User_ID`) REFERENCES `users` (`User_ID`) ON DELETE CASCADE ON UPDATE RESTRICT
+) ENGINE=InnoDB AUTO_INCREMENT=540 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE TABLE purchase (
-   Purchase_ID INTEGER PRIMARY KEY AUTO_INCREMENT,
-   Total_Price DECIMAL(10,2) NOT NULL
-);
+CREATE TABLE `points` (
+  `Points_ID` int NOT NULL AUTO_INCREMENT,
+  `User_ID` int NOT NULL,
+  `Sponsor_ID` int NOT NULL,
+  `Point_Total` int NOT NULL,
+  PRIMARY KEY (`Points_ID`),
+  KEY `points_ibfk_2` (`Sponsor_ID`),
+  KEY `points_ibfk_1` (`User_ID`),
+  CONSTRAINT `points_ibfk_1` FOREIGN KEY (`User_ID`) REFERENCES `users` (`User_ID`) ON DELETE CASCADE ON UPDATE RESTRICT,
+  CONSTRAINT `points_ibfk_2` FOREIGN KEY (`Sponsor_ID`) REFERENCES `sponsor` (`Sponsor_ID`) ON DELETE CASCADE ON UPDATE RESTRICT
+) ENGINE=InnoDB AUTO_INCREMENT=54 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE TABLE item (
-   Item_ID INTEGER PRIMARY KEY AUTO_INCREMENT,
-   Item_Desc VARCHAR(400) NOT NULL,
-   Item_Price Decimal(10,2) NOT NULL
-);
+CREATE TABLE `orders` (
+  `Order_ID` int NOT NULL AUTO_INCREMENT,
+  `User_ID` int NOT NULL,
+  `Sponsor_ID` int NOT NULL,
+  `date_time` datetime NOT NULL,
+  `Status` varchar(20) NOT NULL,
+  `Price` float NOT NULL,
+  `Points` int NOT NULL,
+  `Item_ID` varchar(20) NOT NULL,
+  `Item_Name` varchar(100) NOT NULL,
+  PRIMARY KEY (`Order_ID`),
+  KEY `orders_ibfk_2` (`Sponsor_ID`),
+  KEY `orders_ibfk_1` (`User_ID`),
+  CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`User_ID`) REFERENCES `users` (`User_ID`) ON DELETE CASCADE ON UPDATE RESTRICT,
+  CONSTRAINT `orders_ibfk_2` FOREIGN KEY (`Sponsor_ID`) REFERENCES `sponsor` (`Sponsor_ID`) ON DELETE CASCADE ON UPDATE RESTRICT
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE TABLE purchase_item (
-   Purchase_ID INTEGER NOT NULL,
-   Item_ID INTEGER NOT NULL,
-   Quantity INTEGER NOT NULL,
-   PRIMARY KEY(Purchase_ID, Item_ID),
-   FOREIGN KEY (Purchase_ID) REFERENCES purchase(Purchase_ID),
-   FOREIGN KEY (Item_ID) REFERENCES item(Item_ID)
-);
+CREATE TABLE `password_changes` (
+  `Change_ID` int NOT NULL AUTO_INCREMENT,
+  `Date_Time` datetime NOT NULL,
+  `User_ID` int NOT NULL,
+  `Type_Of_Change` varchar(10) DEFAULT NULL,
+  PRIMARY KEY (`Change_ID`),
+  KEY `password_changes_ibfk_1` (`User_ID`),
+  CONSTRAINT `password_changes_ibfk_1` FOREIGN KEY (`User_ID`) REFERENCES `users` (`User_ID`) ON DELETE CASCADE ON UPDATE RESTRICT
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE TABLE points (
-   Points_ID INTEGER PRIMARY KEY AUTO_INCREMENT,
-   User_ID INTEGER NOT NULL,
-   Sponsor_ID INTEGER NOT NULL,
-   Point_Total INTEGER NOT NULL,
-   FOREIGN KEY  (User_ID) REFERENCES users(User_ID),
-   FOREIGN KEY (Sponsor_ID) REFERENCES sponsor(Sponsor_ID)
-   
-);
+CREATE TABLE `points_history` (
+  `Change_ID` int NOT NULL AUTO_INCREMENT,
+  `User_ID` int NOT NULL,
+  `Sponsor_ID` int NOT NULL,
+  `Point_Change` int NOT NULL,
+  `Date_Time` datetime NOT NULL,
+  `Reason` varchar(50) NOT NULL,
+  `Sponsor_User_ID` int DEFAULT NULL,
+  PRIMARY KEY (`Change_ID`),
+  KEY `points_history_ibfk_2` (`Sponsor_ID`),
+  KEY `points_history_ibfk_1` (`User_ID`),
+  KEY `points_history_ibfk_3` (`Sponsor_User_ID`),
+  CONSTRAINT `points_history_ibfk_1` FOREIGN KEY (`User_ID`) REFERENCES `users` (`User_ID`) ON DELETE CASCADE ON UPDATE RESTRICT,
+  CONSTRAINT `points_history_ibfk_2` FOREIGN KEY (`Sponsor_ID`) REFERENCES `sponsor` (`Sponsor_ID`) ON DELETE CASCADE ON UPDATE RESTRICT,
+  CONSTRAINT `points_history_ibfk_3` FOREIGN KEY (`Sponsor_User_ID`) REFERENCES `users` (`User_ID`) ON DELETE CASCADE ON UPDATE RESTRICT
+) ENGINE=InnoDB AUTO_INCREMENT=80 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE TABLE points_history (
-   Change_ID INTEGER PRIMARY KEY AUTO_INCREMENT,
-   User_ID INTEGER NOT NULL,
-   Sponsor_ID INTEGER NOT NULL,
-   Point_Change INTEGER NOT NULL,
-   Date_Time Datetime NOT NULL,
-   Reason VARCHAR(50) NOT NULL,
-   FOREIGN KEY  (User_ID) REFERENCES users(User_ID),
-   FOREIGN KEY (Sponsor_ID) REFERENCES sponsor(Sponsor_ID)
-   
-);
+CREATE TABLE `application_state_change` (
+  `State_Change_ID` int NOT NULL AUTO_INCREMENT,
+  `Application_ID` int NOT NULL,
+  `Date_Time` datetime NOT NULL,
+  `New_Status` varchar(15) NOT NULL,
+  `New_Reason` varchar(30) DEFAULT NULL,
+  PRIMARY KEY (`State_Change_ID`),
+  KEY `application_state_change_ibfk_1` (`Application_ID`),
+  CONSTRAINT `application_state_change_ibfk_1` FOREIGN KEY (`Application_ID`) REFERENCES `driver_application` (`Application_ID`) ON DELETE CASCADE ON UPDATE RESTRICT
+) ENGINE=InnoDB AUTO_INCREMENT=57 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE TABLE points_old (
-   Transaction_ID INTEGER PRIMARY KEY AUTO_INCREMENT,
-   User_ID INTEGER NOT NULL,
-   Point_Total INTEGER NOT NULL,
-   Points_Added_Or_Deducted INTEGER NOT NULL,
-   Date_Time DATETIME NOT NULL,
-   Reason VARCHAR(30) NOT NULL,
-   Purchase_ID INTEGER,
-   FOREIGN KEY  (User_ID) REFERENCES driver_user(User_ID),
-   FOREIGN KEY  (Purchase_ID) REFERENCES purchase(Purchase_ID)
-);
+/* Django auto-generated tables through migration */
+CREATE TABLE `auth_group` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(150) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE TABLE application_state_change (
-	State_Change_ID INTEGER PRIMARY KEY AUTO_INCREMENT,
-	Application_ID INT NOT NULL,
-	Date_Time DATETIME NOT NULL,
-	New_Status VARCHAR(15) NOT NULL,
-	New_Reason VARCHAR(30),
-	FOREIGN KEY (Application_ID) REFERENCES driver_application(Application_ID)
-);
+CREATE TABLE `auth_group_permissions` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `group_id` int NOT NULL,
+  `permission_id` int NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `auth_group_permissions_group_id_permission_id_0cd325b0_uniq` (`group_id`,`permission_id`),
+  KEY `auth_group_permissio_permission_id_84c5c92e_fk_auth_perm` (`permission_id`),
+  CONSTRAINT `auth_group_permissio_permission_id_84c5c92e_fk_auth_perm` FOREIGN KEY (`permission_id`) REFERENCES `auth_permission` (`id`),
+  CONSTRAINT `auth_group_permissions_group_id_b120cbf9_fk_auth_group_id` FOREIGN KEY (`group_id`) REFERENCES `auth_group` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE TABLE password_changes (
-	Change_ID INT PRIMARY KEY AUTO_INCREMENT,
-	Date_Time datetime NOT NULL,
-	User_ID int NOT NULL,
-	Type_Of_Change VARCHAR(10),
-	FOREIGN KEY (User_ID) REFERENCES users(User_ID)
-);
+CREATE TABLE `auth_permission` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `content_type_id` int NOT NULL,
+  `codename` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `auth_permission_content_type_id_codename_01ab375a_uniq` (`content_type_id`,`codename`),
+  CONSTRAINT `auth_permission_content_type_id_2f476e4b_fk_django_co` FOREIGN KEY (`content_type_id`) REFERENCES `django_content_type` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=69 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
+CREATE TABLE `django_admin_log` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `action_time` datetime(6) NOT NULL,
+  `object_id` longtext,
+  `object_repr` varchar(200) NOT NULL,
+  `action_flag` smallint unsigned NOT NULL,
+  `change_message` longtext NOT NULL,
+  `content_type_id` int DEFAULT NULL,
+  `user_id` int NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `django_admin_log_content_type_id_c4bce8eb_fk_django_co` (`content_type_id`),
+  KEY `django_admin_log_user_id_c564eba6_fk_users_User_ID` (`user_id`),
+  CONSTRAINT `django_admin_log_content_type_id_c4bce8eb_fk_django_co` FOREIGN KEY (`content_type_id`) REFERENCES `django_content_type` (`id`),
+  CONSTRAINT `django_admin_log_user_id_c564eba6_fk_users_User_ID` FOREIGN KEY (`user_id`) REFERENCES `users` (`User_ID`),
+  CONSTRAINT `django_admin_log_chk_1` CHECK ((`action_flag` >= 0))
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE TABLE orders (
-   Order_ID INTEGER NOT NULL AUTO_INCREMENT,
-   User_ID INTEGER NOT NULL,
-   Sponsor_ID INTEGER NOT NULL,
-   date_time DATETIME NOT NULL,
-   Status VARCHAR(20) NOT NULL,
-   Item_ID VARHCAHR(20) NOT NULL,
-   Price FLOAT NOT NULL,
-   Points INT NOT NULL,
-   PRIMARY KEY(Order_ID),
-   FOREIGN KEY (User_ID) REFERENCES users(User_ID),
-   FOREIGN KEY (Sponsor_ID) REFERENCES sponsor(Sponsor_ID)
-);
+CREATE TABLE `django_content_type` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `app_label` varchar(100) NOT NULL,
+  `model` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `django_content_type_app_label_model_76bd3d3b_uniq` (`app_label`,`model`)
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `django_migrations` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `app` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `applied` datetime(6) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `django_session` (
+  `session_key` varchar(40) NOT NULL,
+  `session_data` longtext NOT NULL,
+  `expire_date` datetime(6) NOT NULL,
+  PRIMARY KEY (`session_key`),
+  KEY `django_session_expire_date_a5c62663` (`expire_date`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
--- Database/ViewTables.sql
+++ Database/ViewTables.sql
@@ -1,14 +1,127 @@
 USE Good_Driver;
 
-SELECT * FROM sponsor;
-SELECT * FROM login_attempt;
-SELECT * FROM users;
-SELECT * FROM admin_user;
-SELECT * FROM driver_user;
-SELECT * FROM sponsor_user;
-SELECT * FROM driver_application;
-SELECT * FROM driver_sponsor;
-SELECT * FROM points;
-SELECT * FROM purchase;
-SELECT * FROM purchase_item;
-SELECT * FROM item;
+SELECT `application_state_change`.`State_Change_ID`,
+    `application_state_change`.`Application_ID`,
+    `application_state_change`.`Date_Time`,
+    `application_state_change`.`New_Status`,
+    `application_state_change`.`New_Reason`
+FROM `Good_Driver`.`application_state_change`;
+
+SELECT `auth_group`.`id`,
+    `auth_group`.`name`
+FROM `Good_Driver`.`auth_group`;
+
+SELECT `auth_group_permissions`.`id`,
+    `auth_group_permissions`.`group_id`,
+    `auth_group_permissions`.`permission_id`
+FROM `Good_Driver`.`auth_group_permissions`;
+
+SELECT `auth_permission`.`id`,
+    `auth_permission`.`name`,
+    `auth_permission`.`content_type_id`,
+    `auth_permission`.`codename`
+FROM `Good_Driver`.`auth_permission`;
+
+SELECT `django_admin_log`.`id`,
+    `django_admin_log`.`action_time`,
+    `django_admin_log`.`object_id`,
+    `django_admin_log`.`object_repr`,
+    `django_admin_log`.`action_flag`,
+    `django_admin_log`.`change_message`,
+    `django_admin_log`.`content_type_id`,
+    `django_admin_log`.`user_id`
+FROM `Good_Driver`.`django_admin_log`;
+
+SELECT `django_content_type`.`id`,
+    `django_content_type`.`app_label`,
+    `django_content_type`.`model`
+FROM `Good_Driver`.`django_content_type`;
+
+SELECT `django_migrations`.`id`,
+    `django_migrations`.`app`,
+    `django_migrations`.`name`,
+    `django_migrations`.`applied`
+FROM `Good_Driver`.`django_migrations`;
+
+SELECT `django_session`.`session_key`,
+    `django_session`.`session_data`,
+    `django_session`.`expire_date`
+FROM `Good_Driver`.`django_session`;
+
+SELECT `driver_application`.`Application_ID`,
+    `driver_application`.`Driver_ID`,
+    `driver_application`.`Sponsor_ID`,
+    `driver_application`.`Date_Time`,
+    `driver_application`.`Status`,
+    `driver_application`.`Reason`
+FROM `Good_Driver`.`driver_application`;
+
+SELECT `driver_sponsor`.`ID`,
+    `driver_sponsor`.`User_ID`,
+    `driver_sponsor`.`Sponsor_ID`
+FROM `Good_Driver`.`driver_sponsor`;
+
+SELECT `login_attempt`.`Attempt_ID`,
+    `login_attempt`.`User_ID`,
+    `login_attempt`.`Date_Time`,
+    `login_attempt`.`Was_Accepted`
+FROM `Good_Driver`.`login_attempt`;
+
+SELECT `orders`.`Order_ID`,
+    `orders`.`User_ID`,
+    `orders`.`Sponsor_ID`,
+    `orders`.`date_time`,
+    `orders`.`Status`,
+    `orders`.`Price`,
+    `orders`.`Points`,
+    `orders`.`Item_ID`,
+    `orders`.`Item_Name`
+FROM `Good_Driver`.`orders`;
+
+SELECT `password_changes`.`Change_ID`,
+    `password_changes`.`Date_Time`,
+    `password_changes`.`User_ID`,
+    `password_changes`.`Type_Of_Change`
+FROM `Good_Driver`.`password_changes`;
+
+SELECT `points`.`Points_ID`,
+    `points`.`User_ID`,
+    `points`.`Sponsor_ID`,
+    `points`.`Point_Total`
+FROM `Good_Driver`.`points`;
+
+SELECT `points_history`.`Change_ID`,
+    `points_history`.`User_ID`,
+    `points_history`.`Sponsor_ID`,
+    `points_history`.`Point_Change`,
+    `points_history`.`Date_Time`,
+    `points_history`.`Reason`,
+    `points_history`.`Sponsor_User_ID`
+FROM `Good_Driver`.`points_history`;
+
+SELECT `sponsor`.`Sponsor_ID`,
+    `sponsor`.`Point_Value`,
+    `sponsor`.`Name`,
+    `sponsor`.`Auto_Points`,
+    `sponsor`.`Max_Price`
+FROM `Good_Driver`.`sponsor`;
+
+SELECT `sponsor_user`.`User_ID`,
+    `sponsor_user`.`Sponsor_ID`
+FROM `Good_Driver`.`sponsor_user`;
+
+SELECT `users`.`User_ID`,
+    `users`.`First_Name`,
+    `users`.`Last_Name`,
+    `users`.`Street_Address`,
+    `users`.`Street_Address_2`,
+    `users`.`City`,
+    `users`.`ZIP_Code`,
+    `users`.`Phone_Number`,
+    `users`.`User_Type`,
+    `users`.`Email`,
+    `users`.`Password`,
+    `users`.`last_login`,
+    `users`.`is_impersonation`,
+    `users`.`Security_Question_Answer`
+FROM `Good_Driver`.`users`;
